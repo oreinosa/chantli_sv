b471449e5e4d86db5e8baa61a6205371e85d61b4
Oscar Reinosa <oscar.reinosa96@gmail.com>
2018-04-26 17:43:45 -0500

    added sort

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { CategoriesService } from './categories.service';
 import { Table } from '../../shared/classes/table';
 import { Category } from '../../shared/classes/category';
-import { MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-categories',
@@ -11,9 +10,6 @@ import { MatPaginator, MatSort } from '@angular/material';
 })
 export class CategoriesComponent extends Table<Category>  {
   public displayedColumns = ['name', 'description', 'actions'];
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private categoriesService: CategoriesService,
