d3a8546bdffdacf48442758dc2535893b9c23817
Oscar Reinosa <oscar.reinosa96@gmail.com>
2018-04-26 17:43:29 -0500

    updated table class, added sort method

--- src/app/shared/classes/table.ts
+++ src/app/shared/classes/table.ts
@@ -1,5 +1,5 @@
 import { OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatSort, MatTableDataSource, Sort } from '@angular/material';
 import { Subject } from 'rxjs/Subject';
 import { DAO } from './dao';
 import "rxjs/add/operator/takeUntil";
@@ -7,10 +7,14 @@ import "rxjs/add/operator/takeUntil";
 export class Table<T> implements OnInit, AfterViewInit {
   public ngUnsubscribe = new Subject();
   public displayedColumns?: string[];
-  public sort?: MatSort;
-  public paginator?: MatPaginator;
+  // public sort?: MatSort;
+  // public paginator?: MatPaginator;
+  public data: T[];
   public dataSource: MatTableDataSource<T> = new MatTableDataSource();
   public loaded: boolean = false;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private service: DAO<T>,
@@ -18,7 +22,7 @@ export class Table<T> implements OnInit, AfterViewInit {
 
   onAction(object: T) {
     console.log('action ', object);
-      this.service.object.next(object);
+    this.service.object.next(object);
   }
 
   ngOnInit() {
@@ -29,7 +33,9 @@ export class Table<T> implements OnInit, AfterViewInit {
       .takeUntil(this.ngUnsubscribe)
       .do(data => console.log('Table data : ', data))
       .subscribe(data => {
-        this.dataSource.data = data;
+        this.data = data
+        // this.dataSource.data.length ? false : this.dataSource.data = data;
+        this.sortData(null);
         this.loaded = true;
       });
   }
@@ -43,5 +49,13 @@ export class Table<T> implements OnInit, AfterViewInit {
     // console.log('destroy table');
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
+  }
+
+  sortData(sort: Sort) {
+    this.dataSource.data = this.data.slice();
+  }
+
+  compare(a, b, isAsc) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 }
