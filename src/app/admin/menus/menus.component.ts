5f7a588eb2e31d3903745363778a8529cfbcd79d
Oscar Reinosa <oscar.reinosa96@gmail.com>
2018-04-27 09:48:19 -0500

    added menus table

--- src/app/admin/menus/menus.component.ts
+++ src/app/admin/menus/menus.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Table } from '../../shared/classes/table';
+import { Menu } from '../../shared/classes/menu';
+import { MenusService } from './menus.service';
 
 @Component({
   selector: 'app-menus',
   templateUrl: './menus.component.html',
   styleUrls: ['./menus.component.css']
 })
-export class MenusComponent implements OnInit {
+export class MenusComponent extends Table<Menu>  {
+  public displayedColumns = ['name', 'description', 'actions'];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private menusService: MenusService,
+  ) {
+    super(menusService);
   }
 
 }
